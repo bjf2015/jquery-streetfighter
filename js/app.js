9eb15ec6a878acf39a694847a2d89a487c410bbd
bjf2015 <bfranzoi@icloud.com>
2016-07-18 13:27:17 -0700

    Finished Project

--- js/app.js
+++ js/app.js
@@ -8,19 +8,28 @@ $(document).ready(function() {
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show().animate( 
+		$('.hadouken').finish().show()
+		.animate( 
 			{'left': '1020px'},
 			500,
 			function() {
 				$('.hadouken').hide();
 				$('.hadouken').css('left', '520px');
 			}
-	);
+		);
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	})
 });
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+	}
+
