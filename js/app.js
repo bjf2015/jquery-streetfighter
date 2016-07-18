abc993fb3d2801c7b83cee0b5c70d2cf28437724
justinmarkhamlawrence <justinmarkhamlawrence@gmail.com>
2016-07-18 16:02:03 -0400

    js initial commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show().animate( 
+			{'left': '1020px'},
+			500,
+			function() {
+				$('.hadouken').hide();
+				$('.hadouken').css('left', '520px');
+			}
+	);
+	})
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	})
+});
